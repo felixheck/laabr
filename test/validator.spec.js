e8289da49760356f1e871114e0e92ab5e1ee9040
Felix Heck <hi@whoTheHeck.de>
2017-10-25 08:31:45 +0200

    merge specs

--- test/validator.spec.js
+++
@@ -1,6 +0,0 @@
-const test = require('ava')
-const validator = require('../src/validator')
-
-test('return value if no validator is defined', (t) => {
-  t.is(validator('foobar', 'foobar'), 'foobar')
-})
