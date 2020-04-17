d3e9ae470921dc85ed31e4e3b2138c0bb349d4d1
Felix Heck <hi@whotheheck.de>
2020-04-17 10:36:20 +0200

    cleanup validation

--- src/validator.js
+++ src/validator.js
@@ -7,67 +7,55 @@ const joi = require('@hapi/joi')
  * Collection of `Joi` validators
  */
 const validators = {
-  formatLabel: joi.string().valid('log', 'request', 'response', 'request-error', 'onPostStart', 'onPostStop', 'uncaught'),
+  formatLabel: joi
+    .string()
+    .valid(
+      'log',
+      'request',
+      'response',
+      'request-error',
+      'onPostStart',
+      'onPostStop',
+      'uncaught'
+    ),
   tokenLabel: joi.string(),
   format: joi.alternatives().try(joi.string(), joi.any().valid(false)),
   token: joi.function().maxArity(3),
   preformatterOutput: joi.object(),
   postformatterOutput: joi.string(),
-  options: joi.object({
-    formats: joi.object().allow(null),
-    presets: joi.object().allow(null),
-    tokens: joi.object().allow(null),
-    colored: joi.boolean().default(false),
-    override: joi.boolean().default(false),
-    indent: joi.alternatives().try(joi.number(), joi.string()).allow('').default(2),
-    preformatter: joi.function().maxArity(2),
-    postformatter: joi.function().maxArity(2),
-    handleUncaught: joi.boolean().default(false),
-    stream: joi.object().allow(null),
-    hapiPino: joi.object({
+  options: joi
+    .object({
+      formats: joi.object().allow(null),
+      presets: joi.object().allow(null),
+      tokens: joi.object().allow(null),
+      colored: joi.boolean().default(false),
+      override: joi.boolean().default(false),
+      indent: joi
+        .alternatives()
+        .try(joi.number(), joi.string())
+        .allow('')
+        .default(2),
+      preformatter: joi.function().maxArity(2),
+      postformatter: joi.function().maxArity(2),
+      handleUncaught: joi.boolean().default(false),
       stream: joi.object().allow(null),
-      prettyPrint: joi.boolean(),
-      tags: joi.object(),
-      allTags: joi.string(),
-      instance: joi.object(),
-      logEvents: joi.alternatives().try(joi.array().items(joi.string()), joi.any().allow(false, null)),
-      mergeHapiLogData: joi.boolean().default(false),
-      serializers: joi.object(),
-      logRouteTags: joi.boolean(),
-      logRequestStart: joi.boolean(),
-      logRequestComplete: joi.boolean(),
-      ignorePaths: joi.array().items(joi.string()),
-      ignoreTags: joi.array().items(joi.string()),
-      redact: joi.array().items(joi.string()),
-      logPayload: joi.boolean().default(true),
-      getChildBindings: joi.function(),
-      level: joi.string()
-    }).default(),
-    pino: joi.object({
-      safe: joi.boolean(),
-      name: joi.string(),
-      serializers: joi.object(),
-      timestamp: joi.alternatives().try(joi.function(), joi.boolean()),
-      slowtime: joi.boolean(),
-      extreme: joi.boolean(),
-      level: joi.string(),
-      levelVal: joi.number(),
-      customLevels: joi.object(),
-      redact: joi.alternatives().try(joi.array(), joi.object()),
-      prettyPrint: joi.any(),
-      messageKey: joi.string().default('msg'),
-      changeLevelName: joi.string(),
-      useLevelLabels: joi.boolean(),
-      useOnlyCustomLevels: joi.boolean(),
-      onTerminated: joi.function(),
-      enabled: joi.boolean(),
-      browser: joi.any(),
-      nestedKey: joi.any(),
-      mixin: joi.any(),
-      base: joi.object(),
-      crlf: joi.boolean()
-    }).default()
-  }).default()
+      hapiPino: joi
+        .object({
+          stream: joi.object().allow(null),
+          logEvents: joi
+            .alternatives()
+            .try(joi.array().items(joi.string()), joi.any().allow(false, null)),
+          mergeHapiLogData: joi.boolean().default(false),
+          logPayload: joi.boolean().default(true)
+        })
+        .default(),
+      pino: joi
+        .object({
+          messageKey: joi.string().default('msg')
+        })
+        .default()
+    })
+    .default()
 }
 
 /**
